c00a2643c4581835e2773069b5692ee6507ee676
Macvhil Abrenica <macs.abrenica.au@phinmaed.com>
2026-09-22 09:22:13 +0800

    P2 ni Aki

---
+++ P2.py
@@ -0,0 +1,29 @@
+print("---STUDENT ACTIVITY SCORE SYSTEM---")
+
+Students = int(input("How many students? "))
+
+def Student():
+    S_1= str(input("Enter name: "))
+    A_1 = float(input("Activity 1: "))
+    A_2 = float(input("Activity 2: "))
+    A_3 = float(input("Activity 3: "))
+
+    return S_1, (A_1 + A_2 + A_3) / 3
+
+def status(Average):
+    if Average >= 90:
+        return "Excellent"
+    elif Average >=80:
+        return "Very Good"
+    elif Average >= 75:
+        return "Passed"
+    else:
+        return "Failed"
+
+for i in range(Students):
+    S_1, Average = Student()
+    Status = status(Average)
+
+    print("Student: ", S_1)
+    print("Average:", round(Average, 2))
+    print("Status:", Status)
